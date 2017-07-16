41f1f9d1e1bbe649b08b7fb54ab3d6e84fe51df7
Kirill Krasnozhenov <krasnozhenovk@gmail.com>
2017-07-16 19:40:59 +0300

    changed the toggle to the FadeIn(Out)

--- menu-script.js
+++ menu-script.js
@@ -2,23 +2,20 @@ var OpenButton = document.getElementById("menu-button"),
   CloseButton = document.getElementById("menu-close"),
   CloseZone = document.getElementById("zone-close");
 
-OpenButton.addEventListener("click", function(){
-    $("#overlay-visible").addClass("content-visible");
-});
-CloseButton.addEventListener("click", function(){
-    $("#overlay-visible").removeClass("content-visible");
-});
-jQuery(function($){
-  $(CloseZone).mouseup(function (e){
-    var div = $("#menu-content");
-    if (!div.is(e.target) && div.has(e.target).length === 0) {
-    $("#overlay-visible").removeClass("content-visible");
-    }
-  });
-});
 $(document).ready(function(){
-    $("zone-close").click(function(){
-        $("#menu-content").toggle("slow");
+  $(OpenButton).click(function(){
+    $("#overlay-visible").fadeIn(300);
+  });
+  $(CloseButton).click(function(){
+    $('#overlay-visible').fadeOut(300);
+  });
+   jQuery(function($){
+    $(CloseZone).click(function (e){
+      var div = $("#menu-content");
+      if (!div.is(e.target) && div.has(e.target).length === 0) {
+      $("#overlay-visible").fadeOut(300);
+      }
     });
+  });
 });
 
